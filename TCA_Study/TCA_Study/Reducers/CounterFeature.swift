119bcc3cc2139fc37d9024d3cee88e841b04e19f
PecanPie <hidra0321@naver.com>
2023-12-29 23:00:21 +0900

    Added Side Effect regarding API, using Effect

--- TCA_Study/TCA_Study/Reducers/CounterFeature.swift
+++ TCA_Study/TCA_Study/Reducers/CounterFeature.swift
@@ -14,11 +14,15 @@ import ComposableArchitecture
 struct CounterFeature: Reducer {
     public struct State: Equatable {
         var count = 0
+        var isLoading = false
+        var fact: String?
     }
     
     enum Action {
         case decrementButtonDidTap
         case incrementButtonDidTap
+        case factButtonDidTap
+        case factResponse(String)
     }
     
     var body: some ReducerOf<Self> {
@@ -26,9 +30,26 @@ struct CounterFeature: Reducer {
             switch action {
             case .decrementButtonDidTap:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonDidTap:
                 state.count += 1
+                state.fact = nil
+                return .none
+            case .factButtonDidTap:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                } catch: { error, send in
+                    print(error)
+                }
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
         }
