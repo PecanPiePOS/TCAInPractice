6a0c582842d12521e7b75c9453c32c018a7d4ff3
PecanPie <hidra0321@naver.com>
2023-12-29 14:44:15 +0900

    Removed WithViewStore and added to variable

--- TCA_Study/TCA_Study/Views/CounterView.swift
+++ TCA_Study/TCA_Study/Views/CounterView.swift
@@ -9,47 +9,58 @@ import SwiftUI
 
 import ComposableArchitecture
 
-extension CounterFeature.State: Equatable {}
-
 struct CounterView: View {
     let store: StoreOf<CounterFeature>
+    @ObservedObject var viewStore: ViewStoreOf<CounterFeature>
+    
+    init(store: StoreOf<CounterFeature>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store, observe: { $0 })
+    }
     
     var body: some View {
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            ZStack {
-                Color.black
-                
-                VStack {
-                    Text("\(viewStore.count)")
-                        .font(.largeTitle)
-                        .frame(width: 50)
-                        .padding()
-                        .foregroundStyle(.white)
-                        .background(Color.orange.opacity(1.0))
-                        .cornerRadius(10)
-                    HStack {
-                        Button("-") {
-                            viewStore.send(.decrementButtonDidTap)
-                        }
-                        .font(.largeTitle)
-                        .frame(width: 20)
-                        .padding()
-                        .background(Color.white.opacity(0.1))
-                        .cornerRadius(10)
-                        
-                        
-                        Button("+") {
-                            viewStore.send(.incrementButtonDidTap)
-                        }
-                        .font(.largeTitle)
-                        .frame(width: 20)
-                        .padding()
-                        .background(Color.white.opacity(0.1))
-                        .cornerRadius(10)
+        ZStack {
+            Color.black
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .frame(width: 50)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.orange.opacity(1.0))
+                    .cornerRadius(10)
+                HStack {
+                    Button("-") {
+                        viewStore.send(.decrementButtonDidTap)
                     }
+                    .font(.largeTitle)
+                    .frame(width: 20)
+                    .padding()
+                    .background(Color.white.opacity(0.1))
+                    .cornerRadius(10)
+                    
+                    
+                    Button("+") {
+                        viewStore.send(.incrementButtonDidTap)
+                    }
+                    .font(.largeTitle)
+                    .frame(width: 20)
+                    .padding()
+                    .background(Color.white.opacity(0.1))
+                    .cornerRadius(10)
                 }
             }
-            .ignoresSafeArea()
         }
+        .ignoresSafeArea()
     }
+}
+
+struct CounterPreview: PreviewProvider {
+  static var previews: some View {
+    CounterView(
+      store: Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+      }
+    )
+  }
 }
