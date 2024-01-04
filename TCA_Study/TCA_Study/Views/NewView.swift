45f12d40ed0208a8ee943879dda13b87a6499848
PecanPie <hidra0321@naver.com>
2024-01-04 19:55:26 +0900

    --

--- TCA_Study/TCA_Study/Views/NewView.swift
+++ TCA_Study/TCA_Study/Views/NewView.swift
@@ -7,12 +7,9 @@
 
 import SwiftUI
 
+/// Combine Added Test
 struct NewView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}
-
-#Preview {
-    NewView()
 }
