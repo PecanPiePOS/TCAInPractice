85efca3af2f40e5d40b85891e34bdd86f0c05257
PecanPie <hidra0321@naver.com>
2024-01-04 19:54:21 +0900

    -

---
+++ TCA_Study/TCA_Study/Views/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  TCA_Study
+//
+//  Created by KYUBO A. SHIM on 1/6/24.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
