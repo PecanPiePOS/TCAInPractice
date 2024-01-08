54bd83401fee9e4d4836d18983add2ebc96b424a
PecanPie <hidra0321@naver.com>
2024-01-08 11:46:35 +0900

    Test Network Request with Custom Dependency

--- TCA_Study/TCA_StudyTests/FeatureTest.swift
+++ TCA_Study/TCA_StudyTests/FeatureTest.swift
@@ -13,6 +13,31 @@ import ComposableArchitecture
 @MainActor
 final class CounterFeatureTests: XCTestCase {
     
+    /// 이제 제일 중요한, Network Request 를 Test 해볼건데
+    /// side effcct 케이스 중 가장 흔한 케이스라고 할 수 있다. (서버가 유저의 데이터를 가지고 있기 때문에)
+    /// Testing 하는 난이도가 높다.
+    /// 1. 네트워크 속도가 느릴 수 있다.
+    /// 2. 서버에서 어떤 값이 내려올지 정확히 예측할 수 없기 때문이다.
+    func testNetworkRequest() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.networkData.fetch = { "\($0) is a good number." }
+        }
+        
+        /// 1. fact 버튼을 누른다.
+        /// 2. isLoading 에 반영이 된다.
+        /// 3. 시간이 흐른 뒤, 데이터가 시스템 내부로 들어온다.
+        await store.send(.factButtonDidTap) {
+            $0.isLoading = true
+        }
+        
+        await store.receive(\.factResponse) {
+            $0.isLoading = false
+            $0.fact = "0 is a good number."
+        }
+    }
+    
     func testTimer() async {
         let clock = TestClock()
         
