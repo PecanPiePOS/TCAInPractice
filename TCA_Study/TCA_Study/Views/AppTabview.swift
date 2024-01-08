aa175587a1867a27bf28fa3c028b24b7885d637f
PecanPie <hidra0321@naver.com>
2024-01-08 23:17:17 +0900

    Added New TabView for comparing single Store and multiple store

--- TCA_Study/TCA_Study/Views/AppTabview.swift
+++ TCA_Study/TCA_Study/Views/AppTabview.swift
@@ -1,5 +1,5 @@
 //
-//  NewView.swift
+//  AppTabview.swift
 //  TCA_Study
 //
 //  Created by KYUBO A. SHIM on 1/6/24.
@@ -7,9 +7,32 @@
 
 import SwiftUI
 
+import ComposableArchitecture
+
 /// Combine Added Test
-struct NewView: View {
+struct AppTabview: View {
+    
+    /// 하나의 Store 로 뷰의 모든걸 책임지는 Reducer 를 적용했다.
+    let store: StoreOf<AppFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            TabView {
+                CounterView(store: store.scope(state: \.tab1, action: \.tab1))
+                    .tabItem { Image(systemName: "dial.low") }
+                
+                CounterView(store: store.scope(state: \.tab2, action: \.tab2))
+                    .tabItem { Image(systemName: "slider.horizontal.2.square") }
+            }
+            .tint(.red)
+        }
+        .navigationTitle("Features")
+        .foregroundStyle(.white)
     }
+}
+
+#Preview {
+    AppTabview(store: Store(initialState: AppFeature.State(), reducer: {
+        AppFeature()
+    }))
 }
