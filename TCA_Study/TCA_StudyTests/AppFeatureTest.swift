dd22937a5727cd4401e6a4fd2896c9f4195e7b22
PecanPie <hidra0321@naver.com>
2024-01-08 23:17:37 +0900

    Added Test for appFeature

--- TCA_Study/TCA_StudyTests/AppFeatureTest.swift
+++ TCA_Study/TCA_StudyTests/AppFeatureTest.swift
@@ -1,8 +1,26 @@
 //
-//  AppFeatureTEst.swift
+//  AppFeatureTest.swift
 //  TCA_StudyTests
 //
 //  Created by KYUBO A. SHIM on 1/8/24.
 //
 
-import Foundation
+import XCTest
+
+import ComposableArchitecture
+@testable import TCA_Study
+
+@MainActor
+final class AppFeatureTest: XCTestCase {
+    
+    func testIncrementInFirstTab() async {
+        let store = TestStore(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+        
+        await store.send(.tab1(.incrementButtonDidTap)) {
+            $0.tab1.count = 1
+        }
+    }
+    
+}
