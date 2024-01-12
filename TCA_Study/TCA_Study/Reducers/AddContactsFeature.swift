59e57394f35ef525e72be1473d380b43958c23bb
PecanPie <hidra0321@naver.com>
2024-01-12 17:46:00 +0900

    Added presented feature as child reducer

--- TCA_Study/TCA_Study/Reducers/AddContactsFeature.swift
+++ TCA_Study/TCA_Study/Reducers/AddContactsFeature.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct AddContactsFeature {
+    
+    typealias PersonName = String
+    @Dependency(\.dismiss) var dismiss
+    
+    struct State: Equatable {
+        var contact: ContactModel
+    }
+    
+    enum Action {
+        case cancelButtonDidtap
+        case saveButtonDidTap
+        case setName(PersonName)
+        
+        case delegate(Delegate)
+        enum Delegate: Equatable {
+            case saveContact(ContactModel)
+        }
+    }
+    
+        var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .cancelButtonDidtap:
+                return .run { _ in await self.dismiss() }
+            case .saveButtonDidTap:
+                return .run { [contact = state.contact] send in
+                    await send(.delegate(.saveContact(contact)))
+                    await self.dismiss()
+                }
+            case let .setName(name):
+                state.contact.name = name
+                return .none
+            case .delegate:
+                return .none
+            }
+        }
+    }
+}
