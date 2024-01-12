4534e49c74a2349153c8c0d54a3acb9b1572e4ee
PecanPie <hidra0321@naver.com>
2024-01-12 17:46:41 +0900

    Added ContactFeature

--- TCA_Study/TCA_Study/Reducers/ContactsFeature.swift
+++ TCA_Study/TCA_Study/Reducers/ContactsFeature.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct ContactsFeature {
+    
+    struct State: Equatable {
+        @PresentationState var addContact: AddContactsFeature.State?
+        var contacts: IdentifiedArrayOf<ContactModel> = []
+    }
+    
+    enum Action {
+        case addButtonDidTap
+        case addContact(PresentationAction<AddContactsFeature.Action>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonDidTap:
+                state.addContact = AddContactsFeature.State(
+                    contact: ContactModel(id: UUID(), name: "")
+                )
+                return .none
+            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+                state.contacts.append(contact)
+                return .none
+            case .addContact:
+                return .none
+            }
+        }
+        .ifLet(\.$addContact, action: \.addContact) {
+            AddContactsFeature()
+        }
+    }
+}
