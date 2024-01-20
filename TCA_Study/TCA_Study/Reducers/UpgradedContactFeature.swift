2c91ecc0cf8a66878b1e020d8e57bbf7d8e15c7d
PecanPie <hidra0321@naver.com>
2024-01-21 00:10:54 +0900

    Destination Reducer added inside the Reducer

--- TCA_Study/TCA_Study/Reducers/UpgradedContactFeature.swift
+++ TCA_Study/TCA_Study/Reducers/UpgradedContactFeature.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+/// 새로운 Reducer 를 기존의 Feature 에서 만들자.
+extension ContactsFeature {
+    
+    @Reducer
+    struct Destination {
+        enum State: Equatable {
+            case addContact(AddContactsFeature.State)
+            case alert(AlertState<ContactsFeature.Action.Alert>)
+        }
+        
+        enum Action {
+            case addContact(AddContactsFeature.Action)
+            case alert(ContactsFeature.Action.Alert)
+        }
+        
+        var body: some ReducerOf<Self> {
+            Scope(state: \.addContact, action: \.addContact) {
+                AddContactsFeature()
+            }
+        }
+    }
+}
